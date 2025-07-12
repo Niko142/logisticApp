48a3a8266ea9e07b0f51de06ea89babdcc041770
Niko142 <baya29.03.1977@gmail.com>
2025-07-12 23:53:15 +0400

    Настройка dockerfile и compose для возможного деплоя backend

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
@@ -6,12 +7,29 @@ from routes import route
 
 app = FastAPI()
 
-# Возможность взаимодействия frontend с API
+# Настройка CORS в зависимости от prod или dev
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+IS_PRODUCTION = ENVIRONMENT == "production"
+
+if IS_PRODUCTION:
+    # Для production-сборки
+    allowed_origins = ["https://logistic-app-psi.vercel.app"]
+    allowed_methods = ["GET", "POST", "PUT", "DELETE"]
+else:
+    # Для dev-режима разработки
+    allowed_origins = ["http://localhost:5173"]
+    allowed_methods = ["*"]
+
+# Получаем origins из env
+additional_origins = os.getenv("ADDITIONAL_ORIGINS", "")
+if additional_origins:
+    allowed_origins.extend(additional_origins.split(","))
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=allowed_methods,
     allow_headers=["*"],
 )
 
